b57aec01bf3fdcb9f1e77c580c2350de0414fea0
rhostem <jsyoung@Seoyoungui-MacBook-Pro.local>
2016-12-24 13:06:10 +0900

    컴포넌트만으로 페이지를 구성하는 예시 구성.
    마크다운 파일 래퍼인 wrappers/md.jsx 파일 내부에서 layout을 확인해서 특정 컴포넌트를 렌더링하게 한다.
    pages 폴더 내부에서 md 파일을 하나 생성한 후 frontmatter에 layout값만 맞춰주면 해당 그 컴포넌트가 렌더링되게 된다.
    
    이 방법을 사용하면 어떤 컴포넌트 내부에서 this.props.routes 객체를 통해 모든 페이지의 정보에 접근 가능하므로 태그 목록을 보여주는 페이지 등을 만들 수 있게 된다.
    
    SampleComponent.js 와 pages/sample 참조.

---
+++ components/SamplePage.js
@@ -0,0 +1,26 @@
+import React, { Component, PropTypes } from 'react';
+
+class SamplePage extends Component {
+  static propTypes = {
+  };
+
+  static defaultProps = {
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+  };
+
+  render() {
+    return (
+      <div>
+        <div>sample page without body</div>
+      </div>
+    );
+  }
+}
+
+export default SamplePage;
