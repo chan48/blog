1878cd329c55b48ff0a90e52fb01ce0047a61edb
rhostem <syoung.j@gmail.com>
2017-07-22 01:09:08 +0900

    add 'Noto Sans Kr' font face

--- src/styles/global.js
+++ src/styles/global.js
@@ -11,4 +11,19 @@ injectGlobal`
     'fontFamily': 'Iropke Batang',
     'fontFilePath': '/fonts/IropkeBatangM'
   })}
+  ${fontFace({
+    'fontFamily': 'Noto Sans KR',
+    'fontWeight': 100,
+    'fontFilePath': 'http://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Thin'
+  })}
+  ${fontFace({
+    'fontFamily': 'Noto Sans KR',
+    'fontWeight': 400,
+    'fontFilePath': 'http://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular'
+  })}
+  ${fontFace({
+    'fontFamily': 'Noto Sans KR',
+    'fontWeight': 700,
+    'fontFilePath': 'http://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Bold'
+  })}
 `;
